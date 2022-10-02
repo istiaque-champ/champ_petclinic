b0d6d1f3daf143132099391f6d9b2cecf392951c
Clément Cadieux <cadieuxclement@gmail.com>
2022-10-02 14:31:08 -0400

    Billing Service Fully Reactive (also added update functionality) (also made jacoco coverage 100%)

--- billing-service/src/main/java/com/petclinic/billing/http/BillControllerExceptionHandler.java
+++ billing-service/src/main/java/com/petclinic/billing/http/BillControllerExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.petclinic.billing.http;
 
 import com.petclinic.billing.exceptions.InvalidInputException;
-import com.petclinic.billing.exceptions.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,32 +10,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 @Slf4j
 @RestControllerAdvice
 public class BillControllerExceptionHandler {
     private static final Logger LOG = LoggerFactory.getLogger(BillControllerExceptionHandler.class);
-
-    @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(NOT_FOUND)
-    public HttpErrorInfo handleNotFoundException(ServerHttpRequest request, Exception ex){
-        return createHttpErrorInfo(NOT_FOUND,request,ex);
-    }
-
     @ExceptionHandler(InvalidInputException.class)
     @ResponseStatus(UNPROCESSABLE_ENTITY)
     public HttpErrorInfo handleInvalidInputException(ServerHttpRequest request, Exception ex){
-        return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex);
+        return createHttpErrorInfo(request, ex);
     }
 
-    private HttpErrorInfo createHttpErrorInfo(HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
+    private HttpErrorInfo createHttpErrorInfo(ServerHttpRequest request, Exception ex) {
         final String path = request.getPath().pathWithinApplication().value();
         final String message = ex.getMessage();
 
-        LOG.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
+        LOG.debug("Returning HTTP status: {} for path: {}, message: {}", HttpStatus.UNPROCESSABLE_ENTITY, path, message);
 
-        return new HttpErrorInfo(path, httpStatus, message);
+        return new HttpErrorInfo(path, HttpStatus.UNPROCESSABLE_ENTITY, message);
     }
 }
