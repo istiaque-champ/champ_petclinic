229a8113020cce95db0b1c599a73f20843643626
Dimitri Baldacchino <dimitri.baldacchino@gmail.com>
2022-10-08 23:58:56 -0400

    Just checking if i messed up

--- prescription-service/src/main/java/com/team2/prescriptionservice/PrescriptionServiceApplication.java
+++ prescription-service/src/main/java/com/team2/prescriptionservice/PrescriptionServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PrescriptionServiceApplication {
 
+    //Dimitri was here
     public static void main(String[] args) {
         SpringApplication.run(PrescriptionServiceApplication.class, args);
     }
