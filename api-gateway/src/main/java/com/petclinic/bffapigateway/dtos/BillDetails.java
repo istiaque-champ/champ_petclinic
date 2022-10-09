c3a25e4f039faf244af3c0bd976673d66a4e9c49
Clément Cadieux <cadieuxclement@gmail.com>
2022-10-08 23:14:39 -0400

    Changed @Data to @Setter, @Getter and @NoArgsConstructor to removed unneeded autogenerated methods

--- api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/BillDetails.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/BillDetails.java
@@ -1,11 +1,15 @@
 package com.petclinic.bffapigateway.dtos;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 
-@Data
+@Setter
+@Getter
+@NoArgsConstructor
 public class BillDetails {
     private int billId;
     private Date date;
