0dea3b11dd0b57a24881a9fbd93f2ac15a47f73e
Clément Cadieux <cadieuxclement@gmail.com>
2022-10-08 23:22:49 -0400

    Created BillServiceClientTest

---
+++ api-gateway/src/test/java/com/petclinic/bffapigateway/domainclientlayer/BillServiceClientTest.java
@@ -0,0 +1,35 @@
+package com.petclinic.bffapigateway.domainclientlayer;
+
+import org.junit.jupiter.api.Test;
+
+
+class BillServiceClientTest {
+
+    @Test
+    void getBilling() {
+    }
+
+    @Test
+    void getAllBilling() {
+    }
+
+    @Test
+    void createBill() {
+    }
+
+    @Test
+    void getBillsByVetId() {
+    }
+
+    @Test
+    void getBillsByPetId() {
+    }
+
+    @Test
+    void getBillsByCustomerId() {
+    }
+
+    @Test
+    void deleteBill() {
+    }
+}
