697536b39d3c10e418ae88fb4689212e41b5ea41
Dimitri Baldacchino <dimitri.baldacchino@gmail.com>
2022-10-08 23:34:54 -0400

    Created the prescription-service

---
+++ prescription-service/src/test/java/com/team2/prescriptionservice/PrescriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.prescriptionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescriptionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
