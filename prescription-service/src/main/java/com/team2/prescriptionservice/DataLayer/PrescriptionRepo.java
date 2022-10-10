e90b8895971d97926f3e6f37b85318624c367d99
Dimitri Baldacchino <dimitri.baldacchino@gmail.com>
2022-10-10 00:59:04 -0400

    Added custom finder methods that might be used in the future to the PrescriptionRepo

--- prescription-service/src/main/java/com/team2/prescriptionservice/DataLayer/PrescriptionRepo.java
+++ prescription-service/src/main/java/com/team2/prescriptionservice/DataLayer/PrescriptionRepo.java
@@ -3,4 +3,14 @@ package com.team2.prescriptionservice.DataLayer;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PrescriptionRepo extends CrudRepository<Prescription, Integer> {
+
+        public Prescription findPrescriptionByPrescriptionId(Integer prescriptionId);
+
+        public Prescription findPrescriptionByMedication(String medicationName);
+
+        public boolean existsPrescriptionByPrescriptionId(Integer prescriptionId);
+
+        public void deletePrescriptionByPrescriptionId(Integer prescriptionId);
+
+
 }
