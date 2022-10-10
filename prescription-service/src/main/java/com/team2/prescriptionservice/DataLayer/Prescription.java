79566a0e4cc00cc8862fbe9993513121e6c251ab
Dimitri Baldacchino <dimitri.baldacchino@gmail.com>
2022-10-10 00:05:34 -0400

    Created the Prescription.java model class

---
+++ prescription-service/src/main/java/com/team2/prescriptionservice/DataLayer/Prescription.java
@@ -0,0 +1,38 @@
+package com.team2.prescriptionservice.DataLayer;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "prescription")
+public class Prescription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "medication", nullable = false)
+    private String medication;
+
+    @Column(name = "amount", nullable = false)
+    private String amount;
+
+    @Column(name = "datePrinted")
+    @Temporal(TemporalType.DATE)
+    private Date datePrinted;
+
+    @Column(name = "instructions")
+    private String instructions;
+
+
+}
