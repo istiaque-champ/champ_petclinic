105932f025f22fb8fc99eae76f7e8debe7ff935b
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2022-10-11 10:23:13 -0400

    modify presentation layer

--- prescription-service/src/main/java/com/team2/prescriptionservice/PresentationLayer/PrescriptionRessource.java
+++ prescription-service/src/main/java/com/team2/prescriptionservice/PresentationLayer/PrescriptionRessource.java
@@ -1,4 +1,12 @@
 package com.team2.prescriptionservice.PresentationLayer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/prescriptions")
+@RestController
+@Slf4j
 public class PrescriptionRessource {
+
 }
