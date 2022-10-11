c137656f1c4ce7c4d0a2147a3117b1986a858928
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2022-10-11 17:05:16 -0400

    added exceptions

---
+++ prescription-service/src/main/java/com/team2/prescriptionservice/Exceptions/InvalidInputException.java
@@ -0,0 +1,26 @@
+package com.team2.prescriptionservice.Exceptions;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+
+@ResponseStatus(UNPROCESSABLE_ENTITY)
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(){
+
+    }
+
+    public InvalidInputException(String message){
+        super(message);
+    }
+
+    public InvalidInputException(Throwable cause){
+        super(cause);
+    }
+
+    public InvalidInputException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
+
+
