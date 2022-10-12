aa44f74293b933944f7522ad4fd0b8de6a1c384b
AToner25 <anthonytoner25@gmail.com>
2022-10-11 23:10:04 -0400

    Finished the new format for the details page making it more like a form. Html is essentially done just need to add the dropdown option instead of input. Next need to work on getting the getAll function to retrieve all the information from customer.

--- api-gateway/src/main/resources/static/scripts/bill-details/bill-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/bill-details/bill-details.controller.js
@@ -2,9 +2,25 @@
 
 angular.module('billDetails')
     .controller('BillDetailsController', ['$http', '$stateParams', function ($http, $stateParams) {
-        var self = this;
+        var self = this
+        //var newBillId = this
 
         $http.get('api/gateway/bills/' + $stateParams.billId).then(function (resp) {
             self.bills = resp.data;
-        });
-    }]);
+        })
+
+       /* $http.post('api/gateway/bills/' + $stateParams.newId).then(function (createBillId){
+            newBillId.bills = createBillId.data;
+        })*/
+
+        self.submitBillDetailsForm = function (){
+
+            var uri = 'api/gateway/bills/' + $stateParams.billId
+            $http.put(uri, self.bills)
+        }
+
+       /* self.AddBillForm = function (){
+            var uri = 'api/gateway/bills/' + $$stateParams.newId
+            $http.post(uri, newBillId.bills)
+        }*/
+    }])
