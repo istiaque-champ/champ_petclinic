a05f6c759446a4c94fdaf73d9d3373ed794d3860
AToner25 <anthonytoner25@gmail.com>
2022-10-16 17:37:19 -0400

    Got the Print to show only billing details

--- api-gateway/src/main/resources/static/scripts/bill-details/bill-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/bill-details/bill-details.controller.js
@@ -62,7 +62,13 @@ angular.module('billDetails')
         }
 
         self.printPage = function (){
-            window.print();
+            var prtContent = document.getElementById("details");
+            var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+            WinPrint.document.write(prtContent.innerHTML);
+            WinPrint.document.close();
+            WinPrint.focus();
+            WinPrint.print();
+            WinPrint.close();
         }
 
     }])
