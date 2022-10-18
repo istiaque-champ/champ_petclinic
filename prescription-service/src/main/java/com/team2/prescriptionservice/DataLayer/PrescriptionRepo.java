2ef42860c0fc7cb2145cf91d1c3578de25c39804
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2022-10-18 16:41:51 -0400

    Implementing with ApiGateway

--- prescription-service/src/main/java/com/team2/prescriptionservice/DataLayer/PrescriptionRepo.java
+++ prescription-service/src/main/java/com/team2/prescriptionservice/DataLayer/PrescriptionRepo.java
@@ -3,6 +3,7 @@ package com.team2.prescriptionservice.DataLayer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -18,5 +19,7 @@ public interface PrescriptionRepo extends CrudRepository<Prescription, Integer> 
         void deletePrescriptionByPrescriptionId(Integer prescriptionId);
 
         Prescription findTopByOrderByPrescriptionIdDesc();
+
+        List<Prescription> findPrescriptionsByPetId(Integer petId);
 
 }
