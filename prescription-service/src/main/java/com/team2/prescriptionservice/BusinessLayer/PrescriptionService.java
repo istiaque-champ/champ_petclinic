9485aaaeffd5d19b0ddc1fd710c2d700e700c697
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2022-10-18 16:42:02 -0400

    Implementing with ApiGateway

--- prescription-service/src/main/java/com/team2/prescriptionservice/BusinessLayer/PrescriptionService.java
+++ prescription-service/src/main/java/com/team2/prescriptionservice/BusinessLayer/PrescriptionService.java
@@ -1,14 +1,13 @@
 package com.team2.prescriptionservice.BusinessLayer;
 
-import com.team2.prescriptionservice.DataLayer.Prescription;
 import com.team2.prescriptionservice.DataLayer.PrescriptionRequest;
 import com.team2.prescriptionservice.DataLayer.PrescriptionResponse;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PrescriptionService {
     PrescriptionResponse findByPrescriptionId(int id);
     List<PrescriptionResponse> findAllPrescriptions();
     PrescriptionResponse savePrescription(PrescriptionRequest prescriptionRequest);
+    List<PrescriptionResponse> findAllPrescriptionsByPetId(Integer petId);
 }
