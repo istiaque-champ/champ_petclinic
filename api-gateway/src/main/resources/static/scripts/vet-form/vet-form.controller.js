110cf6df3b51f76d9e9f68e59e9c4c83c946582c
FrancisShehata <83419373+FrancisShehata@users.noreply.github.com>
2022-10-23 13:08:13 -0400

    fixed small errors in the vet-form.js and in the html

--- api-gateway/src/main/resources/static/scripts/vet-form/vet-form.controller.js
+++ api-gateway/src/main/resources/static/scripts/vet-form/vet-form.controller.js
@@ -81,17 +81,6 @@ angular.module('vetForm')
                 console.log(self.vet)
             }
 
-            const image_input = document.querySelector("#image_input");
-            var uploaded_image = " ";
-
-            image_input.addEventListener("change", function(){
-            const reader = new FileReader();
-            reader.addEventListener("load",() => {
-            uploaded_image = reader.result;
-            document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
-            });
-            reader.readDataAsDataURL(this.files[0]);
-            })
 
             req.then(function () {
                 $state.go('vets');
