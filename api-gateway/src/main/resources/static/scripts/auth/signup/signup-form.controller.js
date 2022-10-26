285cbae9dc5ee0cd2b88a8ac8c54ec200630107c
Clément Cadieux <cadieuxclement@gmail.com>
2022-10-26 12:49:35 -0400

    Update Request sends

--- api-gateway/src/main/resources/static/scripts/auth/signup/signup-form.controller.js
+++ api-gateway/src/main/resources/static/scripts/auth/signup/signup-form.controller.js
@@ -7,14 +7,28 @@
 angular.module('signupForm')
     .controller('SignupFormController', ['$http', '$scope', "$location", "$stateParams", function ($http, $scope, $location, $stateParams) {
 
-        var method = $stateParams.method;
+        var signup = $stateParams.method === "signupForm";
+        self.signup = signup;
+
+        this.update = () => $http.put("/api/gateway/users/" + $stateParams.id, {
+            username: $scope.signup.username,
+            password: $scope.signup.password,
+            email: $scope.signup.email,
+        })
+            .then(() => {
+                $location.path("/adminPanel");
+            })
+            .catch(n => {
+                $scope.errorMessages = n.data.message.split`\n`;
+                console.log(n);
+            });
 
         this.add = () => $http.post('/api/gateway/users/', {
             username: $scope.signup.username,
             password: $scope.signup.password,
             email: $scope.signup.email,
         })
-            .then(() => $location.path(method === "signupForm" ? "/login" : "/adminPanel"))
+            .then(() => $location.path(signup ? "/login" : "/adminPanel"))
             .catch(n => {
                 $scope.errorMessages = n.data.message.split`\n`;
                 console.log(n);
