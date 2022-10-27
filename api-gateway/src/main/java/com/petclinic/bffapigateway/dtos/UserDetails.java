672e5d8fe7a102f9aede97d6a363a2dfc9eef381
Clément Cadieux <cadieuxclement@gmail.com>
2022-10-27 10:30:31 -0400

    Remove UserType Enum from Api-Gateway, using Integers

--- api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/UserDetails.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/UserDetails.java
@@ -22,7 +22,7 @@ public class UserDetails {
     private String email;
 
     private Set<Role> roles;
-    private UserType userType;
+    private Integer userType;
     private Integer userTypeId;
 }
 
