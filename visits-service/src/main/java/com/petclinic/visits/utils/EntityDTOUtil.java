6c0b952b5bd6f72531904bae9dcf857821346cc7
VictorSsuto <83074897+VictorSsuto@users.noreply.github.com>
2022-10-29 13:02:41 -0400

    With the help of Carlo we went over the DTO to util class and made changes from the last time i worked on it

--- visits-service/src/main/java/com/petclinic/visits/utils/EntityDTOUtil.java
+++ visits-service/src/main/java/com/petclinic/visits/utils/EntityDTOUtil.java
@@ -1,56 +1,49 @@
 package com.petclinic.visits.utils;
 
-import com.petclinic.visits.Reactive.dataacesslayer.Visits;
-import com.petclinic.visits.Reactive.servicelayer.VisitDTO;
 import com.petclinic.visits.datalayer.Visit;
+import com.petclinic.visits.datalayer.VisitDTO;
 import com.petclinic.visits.datalayer.VisitIdLessDTO;
+import com.petclinic.visits.businesslayer.VisitsServiceImpl;
+
+import com.petclinic.visits.utils.exceptions.InvalidInputException;
 import lombok.Generated;
-import reactor.core.publisher.Mono;
+import org.springframework.beans.BeanUtils;
 
-import java.util.Date;
-import java.util.Random;
+import java.util.UUID;
 
-
-public class EntityDTOUtil {
-
+public class EntityDTOUtil{
   @Generated
-    public EntityDTOUtil(){}
+  public EntityDTOUtil(){}
 
-    public static VisitDTO toDTO(Visits visits){
-
-      VisitDTO dto = new VisitDTO();
-      dto.setVisitId(visits.getVisitId());
-      dto.setDate(visits.getDate());
-      dto.setDescription(visits.getDescription());
-      dto.setPetId(visits.getPetId());
-      dto.setPractitionerId(visits.getPractitionerId());
-      dto.setStatus(visits.isStatus());
-
-      //can i change the pet id and practitioner id to string ?check VisitDTO
-        //finsih up the dtos
-      return dto;
-    }
-
-    public static Visits toEntity (VisitDTO dto){
-
-    Visits visits = new Visits();
-    visits.setVisitId(generateVisitId());
-    visits.setDate(new Date());
-    visits.setPetId(dto.getPetId());
-    visits.setPractitionerId(dto.getPractitionerId());
-    visits.setStatus(dto.isStatus());
-
-
-    return visits;
-    }
-
-  private static String generateVisitId() {
-    Random random = new Random();
-    int number = random.nextInt(99999);
-    return "22" + (String.format("%05d", number));
+  public static VisitDTO entityToDTO(Visit visit){
+    VisitDTO visitDTO = new VisitDTO();
+    BeanUtils.copyProperties(visit, visitDTO);
+    return visitDTO;
   }
 
+  public static Visit dtoToEntity(VisitDTO visitDTO){
+    visitDTO.setVisitId(String.valueOf(UUID.randomUUID()));
 
-  public static Visit dtoToEntity(Mono<VisitIdLessDTO> visit) {
+    //Check Valid IDs
+    if(visitDTO.getPractitionerId() <= 0){
+      throw new InvalidInputException("The practitioner id is invalid");
+    }
+    if(visitDTO.getPetId() <= 0){
+      throw new InvalidInputException("The pet id is invalid");
+    }
+
+    //if(VisitsServiceImpl.)
+
+    Visit visit = new Visit();
+    BeanUtils.copyProperties(visitDTO, visit);
+
+    return visit;
+  }
+
+  public static Integer verifyId(Integer id){
+    if(id <= 0){
+      throw new InvalidInputException("That id is invalid");
+    }
+    return id;
   }
 }
