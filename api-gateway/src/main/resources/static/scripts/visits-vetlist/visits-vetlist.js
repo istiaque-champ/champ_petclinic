aed24ba9ed7a8cefe7aee088acba1e4774b0fe54
Carstal <43147122+Carstal@users.noreply.github.com>
2022-10-29 21:17:45 -0400

    Documenting/Commenting js module file

--- api-gateway/src/main/resources/static/scripts/visits-vetlist/visits-vetlist.js
+++ api-gateway/src/main/resources/static/scripts/visits-vetlist/visits-vetlist.js
@@ -1,11 +1,17 @@
 'use strict';
 
+//Setting up the angular module for Visits-Vetlist.
+//1. Use the same name as the module used in component file.
 angular.module('visitsVetList', ['ui.router'])
     .config(['$stateProvider', function ($stateProvider) {
         $stateProvider
+            //2. Create new state for State Provider
             .state('VisitsVet', {
+                //3. Define name of parent
                 parent: 'app',
+                //4. Define path for page
                 url: '/visits/vets',
+                //5. Define template headers
                 template: '<visits-vetlist></visits-vetlist>'
             })
     }]);
