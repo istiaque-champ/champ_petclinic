79af385620995b92fd5dbd2ff7994e4cf5d0ebe1
Carstal <43147122+Carstal@users.noreply.github.com>
2022-10-29 20:50:56 -0400

    creating component file

---
+++ api-gateway/src/main/resources/static/scripts/visits-vetlist/visits-vetlist.component.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('visitsVetList')
+    .component('visitsVetList', {
+        templateUrl: 'scripts/visits-vetlist/visits-vetlist.template.html',
+        controller: 'VisitsVetListController'
+    });
