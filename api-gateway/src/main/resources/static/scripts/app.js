9c812c7f7b3a4b005fcb41f0d049200a3b07c813
Mohaned <mohanedbouzaidi@gmail.com>
2022-10-30 16:44:45 -0400

    Commented out unessasary code

--- api-gateway/src/main/resources/static/scripts/app.js
+++ api-gateway/src/main/resources/static/scripts/app.js
@@ -9,7 +9,7 @@ const whiteList = new Set([
 var petClinicApp = angular.module('petClinicApp', [
     'ui.router', 'layoutNav', 'layoutFooter', 'layoutWelcome', 'petDetails' , 'ownerList', 'ownerDetails', 'ownerForm', 'petForm'
     , 'visits', 'vetList','vetForm','vetDetails', 'loginForm', 'rolesDetails', 'signupForm', 'billDetails', 'billHistory'
-    , 'verification' , 'adminPanel', 'pascalprecht.translate']);
+    , 'verification' , 'adminPanel', /*'pascalprecht.translate'*/]);
 
 petClinicApp.factory("authProvider", ["$window", function ($window) {
 
@@ -66,7 +66,7 @@ petClinicApp.run(['$rootScope', '$location', 'authProvider', function ($rootScop
         }
     });
 }])
-
+/*
 var visits_translations = {
     UPCOMING_VISITS: 'Upcoming Visits:',
     PREVIOUS_VISITS: 'Previous Visits:',
@@ -92,8 +92,10 @@ var translations = {
 };
 console.table(translations);
 
-petClinicApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', '$translateProvider', function (
-    $stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, $translateProvider) {
+ */
+
+petClinicApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', /*'$translateProvider'*/, function (
+    $stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, /*$translateProvider*/) {
 
     // safari turns to be lazy sending the Cache-Control header
     $httpProvider.defaults.headers.common["Cache-Control"] = 'no-cache';
