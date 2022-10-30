379b36abe172c5fba379f110af86bf81df6a244c
Carstal <43147122+Carstal@users.noreply.github.com>
2022-10-29 20:54:43 -0400

    Documenting/Commenting component file

--- api-gateway/src/main/resources/static/scripts/visits-vetlist/visits-vetlist.component.js
+++ api-gateway/src/main/resources/static/scripts/visits-vetlist/visits-vetlist.component.js
@@ -1,7 +1,12 @@
 'use strict';
 
+//This is the file for the creation of the angular module used.
+//1. The name for the module is defined.
 angular.module('visitsVetList')
+    //2. The component for the module is defined.
     .component('visitsVetList', {
+        //3. The path for the template is defined.
         templateUrl: 'scripts/visits-vetlist/visits-vetlist.template.html',
+        //4.The name of the controller is defined.
         controller: 'VisitsVetListController'
     });
