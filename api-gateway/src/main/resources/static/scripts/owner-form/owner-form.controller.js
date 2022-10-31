8fe6fc7d9d00dfb69e91a66a2c52686b1897f438
MohitShankar <mohitshanker2015@gmail.com>
2022-10-31 13:59:05 -0400

    continuing Validation telling user to enter valid fields

--- api-gateway/src/main/resources/static/scripts/owner-form/owner-form.controller.js
+++ api-gateway/src/main/resources/static/scripts/owner-form/owner-form.controller.js
@@ -38,7 +38,7 @@ angular.module('ownerForm')
                 var error = response.data;
                 error.errors = error.errors || [];
                 alert(error.error + "\r\n" + error.errors.map(function (e) {
-                    return e.field + ": " + "One or many ";
+                    return e.field + ": " + "One or many fields are ";
                 }).join("\r\n"));
             });
         };
